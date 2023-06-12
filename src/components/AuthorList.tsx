bb935933bd769792fbc06c04a3ca2539379a83d1
Marceli Borowczak <marcelborowczak@outlook.com>
2023-06-13 01:56:21 +0200

    Feat: author list

---
+++ src/components/AuthorList.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Loading from "./Loading";
+
+interface Book {
+  volumeInfo: {
+    authors?: string[];
+  };
+}
+
+export default function AuthorList() {
+  const [authors, setAuthors] = useState<string[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAuthors = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://www.googleapis.com/books/v1/volumes?q=inauthor:&maxResults=10"
+        );
+        const data = await response.json();
+        if (data.items) {
+          const authors = data.items
+            .map((item: Book) => item.volumeInfo.authors)
+            .flat()
+            .filter((author: string | undefined) => author !== undefined) as string[];
+          setAuthors(authors);
+        }
+      } catch (error) {
+        console.error("Error fetching authors:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAuthors();
+  }, []);
+
+  const handleLearnMore = (author: string) => {
+    navigate(`/authors/${encodeURIComponent(author)}`);
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 bg-gray-200 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Author</th>
+            <th className="px-6 py-3 bg-gray-200 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {authors.map((author, index) => (
+            <tr key={index}>
+              <td className="px-6 py-4 whitespace-nowrap">{author}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                  onClick={() => handleLearnMore(author)}
+                >
+                  Learn More
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/pages/Authors.tsx
+++ src/pages/Authors.tsx
@@ -1,26 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import AuthorList from "../components/AuthorList";
 import useBreadcrumbUpdate from "../hooks/useBreadcrumbUpdate";
 
 export default function Authors() {
-  useBreadcrumbUpdate();
-  const navigate = useNavigate();
+    useBreadcrumbUpdate();
 
-  const handleGoBack = () => {
-    navigate(-1); // Navigates back to the previous page
-  };
 
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white rounded-lg shadow p-4 text-center">
-        <h1 className="text-2xl font-bold mb-4">404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <button
-          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleGoBack}
-        >
-          Go Back
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-2xl font-bold mb-4 ml-2">Author List</h1>
+            <AuthorList />
+        </div>
+    );
 }
